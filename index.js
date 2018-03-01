ca6fe841b9da397ff19e0780801f4ed345aba666
Joshua Medeski <josh.medeski@gmail.com>
2018-02-28 23:07:52 -0600

    Add basic commander setup

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+const program = require('commander');
+
+program
+  .name('Day Job')
+  .version('1.0.0')
+  .option('-t, --today', 'the amount of hours you\'ve worked today')
+  .option('-w --week', 'the amount of hours you\'ve worked this week (previous to today)')
+  .parse(process.argv);
