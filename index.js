df55a07265fb4c2a114ed5344aacd73de0a5178e
Joshua Medeski <josh.medeski@gmail.com>
2018-02-28 23:23:47 -0600

    Adjust flags and output inputs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const program = require('commander');
 program
   .name('Day Job')
   .version('1.0.0')
-  .option('-t, --today', 'the amount of hours you\'ve worked today')
-  .option('-w --week', 'the amount of hours you\'ve worked this week (previous to today)')
-  .parse(process.argv);
+  .option('-t, --today <today>', 'the amount of hours you\'ve worked today')
+  .option('-w, --week <week>', 'the amount of hours you\'ve worked this week (previous to today)')
+  .parse(process.argv)
+
+console.log('today: %s this week: %s', program.today, program.week);
