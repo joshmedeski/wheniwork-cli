589257e22a6091a42e965f726554e1b8caa9d89b
Josh Medeski <josh.medeski@gmail.com>
2018-09-11 14:23:11 -0500

    Add total hours work to the hours table

--- src/views/hours.table.ts
+++ src/views/hours.table.ts
@@ -1,5 +1,6 @@
 import * as Table from "cli-table";
 import { DateRange } from "../dates/date-range";
+import { Day } from "../dates/day";
 import Formatter from "../formatter";
 
 export class HoursTable {
@@ -8,14 +9,40 @@ export class HoursTable {
 
   constructor(dateRange: DateRange) {
     this.table = new Table({
-      head: ["Day", "Total"].chalkResetBold()
+      head: ["Day", "Hours"].chalkResetBold()
     });
+
     dateRange.days.forEach(day => {
       this.table.push([
         this.format.date(day.date),
         this.format.hours(day.total.worked)
       ]);
     });
+
+    const reduceTotalHoursWorked = (
+      accumulator: Day,
+      currentValue: Day
+    ): number => accumulator.total.worked + currentValue.total.worked;
+
+    this.table.push(
+      [
+        "Total",
+        this.format.hours(this.totalHours(dateRange.days))
+      ].chalkResetBold()
+    );
+
     console.log(this.table.toString());
+  }
+
+  /**
+   * Determines the total number of hours worked from all given days.
+   *
+   * @param days The days to add together
+   * @returns total number of hours worked for all the days.
+   */
+  totalHours(days: Day[]): number {
+    let totalHoursWorked = 0;
+    days.forEach(day => (totalHoursWorked += day.total.worked));
+    return totalHoursWorked;
   }
 }
